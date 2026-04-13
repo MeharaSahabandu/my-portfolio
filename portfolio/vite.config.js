57e99c5241f1c78e202fa173311d5a4dee4c6f1f
MeharaSahabandu <sahabandumehara@gmail.com>
2026-04-13 15:17:18 +0530

    deploy

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/my-portfolio/",
+  server: {
+    port: 3001,
+    open: true,
+  },
+});
